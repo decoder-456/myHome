5cbddf6ac140377d9691305d9827ea3ba28fddba
decoder-456 <raghavkumarj685@gmail.com>
2026-03-22 16:23:57 +0530

    back to basic

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const path = require("path");
+const fs = require("fs");
 const express = require("express");
 const session = require("express-session");
 const multer = require("multer");
@@ -13,6 +14,10 @@ const { authRouter } = require("./routes/authrouter");
 const app = express();
 const MONGO_URI = process.env.MONGO_URI;
 const store = new MongoDBStore({ uri: MONGO_URI, collection: "sessions" });
+const uploadPath = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath);
+}
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -29,7 +34,7 @@ app.use((req, res, next) => {
 });
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(rootDir, "uploads"));
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     console.log("MULTER SESSION:", req.session);
@@ -46,7 +51,7 @@ const fileFilter = (req, file, cb) => {
   }
 };
 app.use(multer({ storage, fileFilter }).single("photoUrl"));
-app.use("/uploads", express.static(path.join(rootDir, "uploads")));
+app.use("/uploads", express.static(uploadPath));
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.urlencoded({ extended: true }));
